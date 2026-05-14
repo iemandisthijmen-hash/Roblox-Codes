f15e7a43dadc0aadd69a285498447d1de543ad55
GoGamerzzYT <iemandisthijmen@gmail.com>
2026-05-14 10:45:28 +0200

    Add header comment for walk speed and jump power script
    Added a header comment explaining the script's functionality.

--- wsandjp.lua
+++ wsandjp.lua
@@ -1,3 +1,5 @@
+--wsandjp.lua - A script you can use in chat to change walk speed (ws) and jump power (jp). Use /walkspeed (number) to change your walk speed to that number, and the same for /jumppower
+
 local player = game.Players.LocalPlayer
 local textChatService = game:GetService("TextChatService")
 
